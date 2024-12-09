8dd69a3afd9346d77dc94d964f4ce4da19aefb2b
junpei ogawa <cornerstone.am3@gmail.com>
2024-12-09 21:04:33 +0900

    update globalStyle

--- src/styles/app.css.ts
+++ src/styles/app.css.ts
@@ -1,6 +1,9 @@
 import { globalStyle } from '@vanilla-extract/css'
 
+import { vars } from './theme.css'
+
 globalStyle('html, body', {
+  color: vars.color.text.black,
   fontFamily: '"Noto Sans JP Variable", sans-serif',
   margin: 0
 })
