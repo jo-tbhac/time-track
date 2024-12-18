1f4097c1d1131bb2c9318738d05e813d48ce9c33
junpei ogawa <cornerstone.am3@gmail.com>
2024-12-18 11:14:02 +0900

    fix IconButton component

--- src/components/ui/IconButton/IconButton.tsx
+++ src/components/ui/IconButton/IconButton.tsx
@@ -1,15 +1,12 @@
-import type { FC, ReactNode } from 'react'
+import type { ComponentProps, FC } from 'react'
 
 import styles from './IconButton.css'
 
-interface Props {
-  children: ReactNode
-  className?: string
-}
+interface Props extends Pick<ComponentProps<'button'>, 'children' | 'className' | 'onClick'> {}
 
-export const IconButton: FC<Props> = ({ children, className }) => {
+export const IconButton: FC<Props> = ({ children, className, onClick }) => {
   return (
-    <button type="button" className={className ?? styles.container}>
+    <button type="button" className={className ?? styles.container} onClick={onClick}>
       {children}
     </button>
   )
