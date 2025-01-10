f1a541b7063dfdcbadd1fa271409328425f09101
junpei ogawa <cornerstone.am3@gmail.com>
2025-01-10 18:19:26 +0900

    remove aws-amplify

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,3 @@
-import { Amplify } from 'aws-amplify'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
@@ -6,15 +5,6 @@ import App from './App.tsx'
 
 import '@fontsource-variable/noto-sans-jp'
 import '@/styles/app.css'
-
-Amplify.configure({
-  Auth: {
-    Cognito: {
-      userPoolClientId: import.meta.env.VITE_COGNITO_USER_POOL_CLIENT_ID,
-      userPoolId: import.meta.env.VITE_COGNITO_USER_POOL_ID
-    }
-  }
-})
 
 const root = document.getElementById('root')
 
