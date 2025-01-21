05f3cc235ee433755199f57d5d443601fe63c97f
junpei ogawa <cornerstone.am3@gmail.com>
2025-01-21 21:43:06 +0900

    add test for update job

--- backend/repository/job_test.go
+++ backend/repository/job_test.go
@@ -32,3 +32,26 @@ func TestShouldSuccessfullyCreateJob(t *testing.T) {
 	assert.Equal(t, job.Name, p.Name)
 	assert.Equal(t, job.HourlyWage, p.HourlyWage)
 }
+
+func TestShouldSuccessfullyUpdateJob(t *testing.T) {
+	db, _ := utils.NewDBMock()
+	defer utils.DeleteTestingDatabase()
+
+	r := NewJobRepository(db)
+	job, err := r.Create(model.CreateJobParams{Name: "test job A", HourlyWage: 5000})
+
+	if err != nil {
+		t.Fatalf("Failed to create job: %v", err)
+	}
+
+	p := model.UpdateJobParams{ID: int(job.ID), Name: "test job B", HourlyWage: 4500}
+
+	job, err = r.Update(p)
+
+	if err != nil {
+		t.Fatalf("Failed to update job: %v", err)
+	}
+
+	assert.Equal(t, job.Name, p.Name)
+	assert.Equal(t, job.HourlyWage, p.HourlyWage)
+}
