e6bf08b22970d5c11a9a3beb56ba7ec43a2f5b10
junpei ogawa <cornerstone.am3@gmail.com>
2025-01-25 23:20:14 +0900

    impl create record function

--- backend/model/params.go
+++ backend/model/params.go
@@ -1,5 +1,7 @@
 package model
 
+import "time"
+
 type CreateJobParams struct {
 	Name       string `json:"name"`
 	HourlyWage int    `json:"hourlyWage"`
@@ -9,4 +11,12 @@ type UpdateJobParams struct {
 	ID         int    `json:"id"`
 	Name       string `json:"name"`
 	HourlyWage int    `json:"hourlyWage"`
+}
+
+type CreateRecordParams struct {
+	StartedAt time.Time  `json:"startedAt"`
+	EndedAt   *time.Time `json:"endedAt"`
+	Note      string     `json:"note"`
+	WorkTime  int        `json:"workTime"`
+	JobID     int        `json:"jobId"`
 }
---
+++ backend/repository/record.go
@@ -0,0 +1,35 @@
+package repository
+
+import (
+	"log"
+	"time-track/backend/model"
+
+	"gorm.io/gorm"
+)
+
+type RecordRepository struct {
+	db *gorm.DB
+}
+
+func NewRecordRepository(db *gorm.DB) RecordRepository {
+	return RecordRepository{db: db}
+}
+
+func (r RecordRepository) Create(p model.CreateRecordParams) (model.Record, error) {
+	record := model.Record{
+		StartedAt: p.StartedAt,
+		EndedAt:   p.EndedAt,
+		Note:      p.Note,
+		WorkTime:  p.WorkTime,
+		JobID:     uint(p.JobID),
+	}
+
+	result := r.db.Create(&record)
+
+	if result.Error != nil {
+		log.Printf("Fail to create record: %v\n", result.Error.Error())
+		return record, result.Error
+	}
+
+	return record, nil
+}
